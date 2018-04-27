51935231fb61c356781b446c63f1aa5dec6a7c74
barreto.ext caio <caio.barreto.ext@sodexo.com>
2018-04-26 21:34:43 -0300

    Versão inicial

---
+++ app/src/main/java/com/example/caiobarretoext/controlebanheiro/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.caiobarretoext.controlebanheiro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+}
